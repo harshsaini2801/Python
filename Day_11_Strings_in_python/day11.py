b90964a799f297c5ac41987ef50becdb5eff8ff6
Harsh <harsh@codewithharry.com>
2026-09-21 22:40:32 +0530

    Python learning

---
+++ Day_11_Strings_in_python/day11.py
@@ -0,0 +1,19 @@
+name = "Harsh"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+apple = '''He said, 
+Hi Harsh
+hey I am good
+"I want to eat an apple'''
+ 
+print("Hello, " + name)
+# print(apple) 
+print(name[0])# Prints the first character of the string like H
+print(name[1])# Prints the second character of the string like a
+print(name[2])# Prints the third character of the string like r
+print(name[3])# Prints the fourth character of the string like s
+print(name[4])# Prints the fifth character of the string like h
+# print(name[5]) # Throws an error
+print("Lets use a for loop\n")# Outputs a new line after the text
+for character in apple:
+    print(character)# Outputs each character of the string apple in a new line
