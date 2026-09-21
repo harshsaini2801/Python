e95a07963eb21d00f7e5beefb027738144520b8c
Harsh <harsh@codewithharry.com>
2026-09-21 22:33:58 +0530

    Python learning

---
+++ Day_02_fun/day2.py
@@ -0,0 +1,5 @@
+'''
+nothing do just show 
+how python makes jarvis 
+how make ai assitant
+'''
---
+++ Day_05_comments_escapes_sequence/day5.py
@@ -0,0 +1,15 @@
+#Comments, Escape Sequences & Print Statement in Python
+#Hey Harsh don't delete this line(THis is a comment line and will not be executed by the python interpreter) 
+print("Hello everyone,\nWelcome to Day 5 of our coding journey!") #"\n" is escape sequence
+print("Hello World !!!") #Printing Hello World
+# """ This is used for multi-line comment """ 
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code.
+"""
+#To print "word" in a string we use escape sequence like this \"word\". It will print "word" in the string.
+print("This is an example of escape sequence \"word\" in a string.")
+
+print("Hey" , 6 , 7, sep = "~") #sep is used to separate the values with a specified character or string. In this case, it will separate the values with "~~".
+print("Hey" , 6 , 7, sep = "-" , end = "009\n")#nd is used to specify what to print at the end of the statement. In this case, it will print "009" at the end of the statement.
+print("Harsh")
